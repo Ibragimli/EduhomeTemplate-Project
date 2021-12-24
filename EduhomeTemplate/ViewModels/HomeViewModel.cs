6dc24c82e160e21bf503c1fdc82b40ff9da6dbda
tu201903193@outlook.com <tu201903193@outlook.com>
2021-12-24 18:42:53 +0400

    Add project files.

---
+++ EduhomeTemplate/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EduhomeTemplate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduhomeTemplate.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Course> Courses { get; set; }
+    }
+}
